49d3bfce89bc1db437335a57ce7f25568fc0ca98
bree <briannadanford1@gmail.com>
2022-12-23 14:53:52 +1100

    Finished

--- README.md
+++ README.md
@@ -1,14 +1,38 @@
-TIC TAC TOE
+# TIC TAC TOE Game
 
-# Process 
+# Project Description 
+For this project, we were asled to make a simple Tic Tac Toe game using the programming languages what we've learnt in class which were HTML, CSS and JavaScript. In building this game application, our main goals were to:
+- Build a web application from scratch, without a starter codebase
+- Use your programming skills to map out the game logic for a simple game like Tic Tac Toe
+- Separate HTML, CSS, and JavaScript files in your application
+- Build an application to a spec that someone else gives you
+- Build a dynamic game that allows two players to compete
+- Craft a README.md file that explains your app to the world
+
+# Technical Requirements
+- Render a game board in the browser
+- Switch turns between X and O (or whichever markers you select)
+- Visually display which side won or show a draw if neither player wins
+- Use JavaScript for DOM manipulation
+- Include separate HTML / CSS / JavaScript files
+- Follow the DRY (Dont' Repeat Yourself) principle
+- Be deployed to GitHub Pages where the rest of the world can access it
+
+# My game applciation process
 - The first thing I did was breaking down the problems into smaller problems. For example, how to make the layout of the game, how to start the game, what's the function for letting the users know who won the game etc. 
 - To begin with, I sketched how I wanted the game to look on paper then designed it on Canva. 
-- Then I started designing the layout and features with HTML and CSS.
-- Then started JavaScript. 
-# Programs 
-- JavaScript 
-- HTML 
-- CSS
-# Problems 
-- No problems so far
+- Then I started designing the layout and features with HTML and CSS. I decided I wanted a gif as a background so I implemented a gif url to the element of body in CSS. I also wanted some optional background music for the user so I added an audio element in html with controls and in JavaScript I added the function to play and pause meaning the user would be able to play/pause and change the volume of the music. 
+- For the JavaScript I just focused on one part of the application as I went along. 
 
+# Features I would implement in the future 
+- A scoreboard to keep track of users points 
+- Customisable Avatars/Icons and customisable names 
+
+# Problems 
+Problems that I encountered in making this game was:
+- Making the click function work
+- Implementing the winning conditions
+- And just doing the whole JavaScript since it's not my strong point. 
+
+# Ending Credits 
+Shout out to the internet. 
--- script.js
+++ script.js
@@ -1,48 +1,101 @@
-// Cell index
+const statusDisplay = document.querySelector(".game-status");
+
+let currentPlayer = "X";
+let gameState = ["", "", "", "", "", "", "", "", ""];
+let running = true; 
+let audio = document.querySelector(".myAudio");
+
+const currentPlayerTurn = () => `${currentPlayer}'s turn`;
+
+statusDisplay.innerHTML = currentPlayerTurn();
+////////////////////////
+// These are the indexes of the cells. First 3 are horizontal, then 
+// next 3 are vertical and last 2 are diagonal. 
 const winningConditions = [
     [0, 1, 2],
     [3, 4, 5],
     [6, 7, 8],
     [0, 3, 6],
-    [1, 4, 7], 
+    [1, 4, 7],
     [2, 5, 8],
-]
-let currentPlayer = "player1";
-const gameState = {
-    winningConditions: winningConditions,
-    currentPlayer: currentPlayer,
-};
-
-// Initialize the game
-function initializeGame(){
-    gameState.winningConditions = ["", "", "", "", "", "", "", "", ""]
-    gameState.currentPlayer = "player1";
+    [0, 4, 8],
+    [2, 4, 6]
+];
+function runCellPlayed(clickedCell, clickedCellIndex) {
+    gameState[clickedCellIndex] = currentPlayer;
+    clickedCell.innerHTML = currentPlayer;
 }
+/////////////////////////
 
-// click function
-const cells = document.querySelectorAll(".cell");
-cells.forEach(cell => {
-    cell.addEventListener('click', handleClick);
-});
-
-function handleClick(event) {
-    const cell = event.target;
-    cell.textContent = currentPlayer;
-    currentPlayer = (currentPlayer === "X") ? "O" : "X"
+// Changing players 
+function runPlayerChange() {
+    currentPlayer = currentPlayer === "X" ? "O" : "X";
+    statusDisplay.innerHTML = currentPlayerTurn();
 }
+////////////////////////
 
-// Players take turns
-function makeMove(row,col) {
-    if (gameState.winningConditions[row][col]) {
-        console.log("Occupied");
+// Check for the winner and if theres a draw + message to let the user know 
+const winner = () => `Player ${currentPlayer} wins!`;
+const draw = () => `Draw!`;
+
+function runResult() {
+    let gameWon = false;
+    for (let i = 0; i <= 7; i++) {
+        const win = winningConditions[i];
+        let a = gameState[win[0]];
+        let b = gameState[win[1]];
+        let c = gameState[win[2]];
+        if (a === '' || b === '' || c === '') {
+            continue;
+        }
+        if (a === b && b === c) {
+            gameWon = true;
+            break
+        }
+    }
+    if (gameWon) {
+        statusDisplay.innerHTML = winner();
+        running = false;
         return;
     }
-    if (gameState.currentPlayer === "player1") {
-        gameState.winningConditions[row][col] = "X";
-    } else {
-        gameState.winningConditions[row][col] = "O";
+    let gameDraw = !gameState.includes("");
+    if (gameDraw) {
+        statusDisplay.innerHTML = draw();
+        running = false;
+        return;
     }
+    runPlayerChange();
 }
+////////////////////////
 
+// Click function for the cells
+function runCellClick(clickedCellEvent) {
+    const clickedCell = clickedCellEvent.target
+    const clickedCellIndex = parseInt(clickedCell.getAttribute('cellIndex'));
+    if (gameState[clickedCellIndex] !== "" || !running) {
+        return;
+    }
+    runCellPlayed(clickedCell, clickedCellIndex);
+    runResult();
+}
+////////////////////////
 
-
+// For the restart button
+function runRestart(){
+    running = true;
+    currentPlayer = "X";
+    gameState = ["", "", "", "", "", "", "", "", ""];
+    statusDisplay.innerHTML = currentPlayerTurn();
+    document.querySelectorAll('.cell').forEach(cell => cell.innerHTML = "");
+}
+////////////////////////
+// For da music
+function playAudio() {
+    audio.play();
+}
+function pauseAudio() {
+    audio.pause();
+}
+/////////////////////////
+document.querySelectorAll('.cell').forEach(cell => cell.addEventListener('click', runCellClick));
+document.querySelector('.restartBtn').addEventListener('click', runRestart);
